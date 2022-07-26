b9597a79cdbb1df50d840d59a5e8c23915723095
Pipe-Smoking-Rabbit <mitchplease02@gmail.com>
2022-07-26 11:36:55 +0100

    more basic styling added to reviews page

--- src/components/reviews-components/AllReviews.jsx
+++ src/components/reviews-components/AllReviews.jsx
@@ -27,16 +27,29 @@ export default function AllReviews() {
           reviewsList.map(review=>{
             return (
               <li className="Review-Card" key={review.review_id}>
-                <h3 className="Review-Card-Title">{review.title}</h3>
-                <h4 className="Review-Card-Category">A {review.category} game</h4>
-                <p className="Review-Card-Peek-Preview">{review.review_body.match(peekPreviewRegex)}...</p>
-                <h5>By {review.owner}</h5>
-                <h5>{review.created_at}</h5>
-                <img src={review.review_img_url}/>
-                <h6>{review.votes} Kudos</h6>
-                <h6>{review.comment_count} Comments</h6>
+                <div className="Review-Card-Info">
+                  <h3 className="Review-Card-Title">{review.title}</h3>
+                  <h4 className="Review-Card-Category">
+                    A {review.category} game
+                  </h4>
+                  <p className="Review-Card-Peek-Preview">
+                    {review.review_body.match(peekPreviewRegex)}...
+                  </p>
+                  <h5 className="Review-Card-Author">By {review.owner}</h5>
+                  <div className="Review-Card-Bottom-Row-Info">
+                    <h6 className="Review-Card-Date">{review.created_at}</h6>
+                    <h6 className="Review-Card-Kudos">{review.votes} Kudos</h6>
+                    <h6 className="Review-Card-Comments">
+                      {review.comment_count} Comments
+                    </h6>
+                  </div>
+                </div>
+                <img
+                  className="Review-Card-Image"
+                  src={review.review_img_url}
+                />
               </li>
-            )
+            );
           })
         }
       </ul>
