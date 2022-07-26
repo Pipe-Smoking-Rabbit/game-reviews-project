9ab64f482da331698e972baedfebc011270fb489
Pipe-Smoking-Rabbit <mitchplease02@gmail.com>
2022-07-26 16:12:59 +0100

    set up review list cards as links to full review page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import Categories from "./components/categories-components/Categories";
 import ReviewsList from "./components/reviews-components/ReviewsList";
+import SingleReview from "./components/reviews-components/SingleReview";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="" element={<ReviewsList />} />
           <Route path="/categories" element={<Categories />} />
           <Route path="/reviews/:category" element={<ReviewsList />} />
+          <Route path="/review/:review_id" element={<SingleReview />} />
         </Routes>
         <NavBar />
       </div>
