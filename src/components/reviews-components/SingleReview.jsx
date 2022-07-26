ab871ecb80c03127c969512dcbbaf24c5134669b
Pipe-Smoking-Rabbit <mitchplease02@gmail.com>
2022-07-26 16:56:52 +0100

    single review component implemented with basic styling

--- src/components/reviews-components/SingleReview.jsx
+++ src/components/reviews-components/SingleReview.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import "./AllReviews.css";
+import "./SingleReview.css";
 const axios = require("axios").default;
 
 export default function SingleReview() {
@@ -17,7 +17,7 @@ export default function SingleReview() {
       .then((response) => {
         setIsLoading(false);
         setReview(response.data.review);
-        console.log(response.data.review)
+        console.log(response.data.review);
       });
   }
 
@@ -25,5 +25,32 @@ export default function SingleReview() {
     fetchSingleReview();
   }, []);
 
-  return <div>SingleReview</div>;
+  if (isLoading)
+    return (
+      <div className="Page-Content Loading-Screen">
+        <img
+          src="https://qph.cf2.quoracdn.net/main-qimg-7a960949a5d51cf8b6ffef964d57feec"
+          alt="Loading reviews..."
+        />
+      </div>
+    );
+  return (
+    <section className="Page-Content Single-Review">
+      <header className="Single-Review-Header">
+        <div className="Single-Review-Header-Info">
+          <h2 className="Single-Review-Title">{review.title}</h2>
+          <h5 className="Single-Review-Category">A {review.category} game</h5>
+          <h7 className="Single-Review-Author">
+            Posted by {review.owner}, on {review.created_at}
+          </h7>
+        </div>
+        <img className="Single-Review-Image" src={review.review_img_url} />
+      </header>
+      <p className="Single-Review-Body">{review.review_body}</p>
+      <h7 className="Single-Review-Kudos">{review.votes} Kudos</h7>
+      <h7 className="Single-Review-Comments">
+        {review.comment_count} Comments
+      </h7>
+    </section>
+  );
 }
