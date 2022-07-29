4c619a8c243cf39149bb2db19c5ff31250837d24
Pipe-Smoking-Rabbit <mitchplease02@gmail.com>
2022-07-29 15:23:07 +0100

    bugs fixed on posting empty comments

--- src/components/comments-components/PostComment.jsx
+++ src/components/comments-components/PostComment.jsx
@@ -3,7 +3,11 @@ import { CurrentUserContext } from "../../contexts/CurrentUser";
 import { postComment } from "../../utils/api";
 import "./Comments.css";
 
-export default function PostComment({ setCommentsList, review_id, setCommentCount }) {
+export default function PostComment({
+  setCommentsList,
+  review_id,
+  setCommentCount,
+}) {
   const {
     currentUser: { username },
   } = useContext(CurrentUserContext);
@@ -11,40 +15,48 @@ export default function PostComment({ setCommentsList, review_id, setCommentCoun
 
   function handleSubmit(event) {
     event.preventDefault();
-    const newComment = {
-      author: username,
-      created_at: "just now",
-      body: newCommentBody,
-      comment_id: Math.random(),
-    };
-    setCommentsList((currentCommentList) => {
-      return [newComment, ...currentCommentList];
-    });
-    postComment(review_id, {
-      username: newComment.author,
-      body: newComment.body,
-    });
-    setNewCommentBody("");
-    setCommentCount((previousCommentCount)=>{
-        return previousCommentCount + 1
-    })
+    if (newCommentBody.length) {
+      const newComment = {
+        author: username,
+        created_at: "just now",
+        body: newCommentBody,
+        comment_id: Math.random(),
+      };
+      setCommentsList((currentCommentList) => {
+        return [newComment, ...currentCommentList];
+      });
+      postComment(review_id, {
+        username: newComment.author,
+        body: newComment.body,
+      });
+      setNewCommentBody("");
+      setCommentCount((previousCommentCount) => {
+        return previousCommentCount + 1;
+      });
+    }
   }
 
   return (
     <section>
       <form className="Post-Comment" onSubmit={handleSubmit}>
-        <label className="Post-Comment-Label" htmlFor="post_comment">{username} -</label>
-        <input
-        className="Post-Comment-Input"
+        <label className="Post-Comment-Label" htmlFor="post_comment">
+          {username} -
+        </label>
+        <textarea
+          className="Post-Comment-Input"
           value={newCommentBody}
           onChange={(event) => {
             setNewCommentBody(event.target.value);
           }}
           type="post_comment"
           placeholder="Add a comment..."
-        ></input>
+        ></textarea>
         {newCommentBody ? (
-          <button className="Post-Comment-Button" type="submit">
+          <button
+            disabled={newCommentBody.length === 0}
+            className="Post-Comment-Button"
+            type="submit"
+          >
             <img
               className="Post-Comment-Button-Image"
               src="https://cdn1.iconfinder.com/data/icons/outline-17/24/send-512.png"
